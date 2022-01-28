290ccaa7dca6bc021594a01047b8ab6ad68bcb5b
krishna bhattad <Bhattadkrishna2@gmail.com>
2022-01-28 22:11:10 +0530

    Updated assignment

--- src/utils/initialization.java
+++ src/utils/initialization.java
@@ -6,20 +6,15 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class initialization {
     WebDriver driver;
+
     public WebDriver initialize() throws Exception {
-        System.setProperty("webdriver.chrome.driver","/home/krishna/IdeaProjects/selenium/src/utils/chromedriver");
-        this.driver=new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "/home/krishna/IdeaProjects/selenium/src/utils/chromedriver");
+        this.driver = new ChromeDriver();
         driver.manage().window().maximize();
         return driver;
 //        driver.get("https://www.google.com");
 //        driver.findElement(By.xpath("//input[@name=\"q\"]")).sendKeys("testing");
     }
-    public void initializeWait() throws Exception{
-        WebDriverWait wait = new WebDriverWait(driver,10);
 
-    }
-    public void teardown(){
-        driver.quit();
 
-    }
 }
